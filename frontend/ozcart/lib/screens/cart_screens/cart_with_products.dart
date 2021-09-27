56f94a18ed447f70a23a3818ea9d995f128fda99
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2021-09-27 07:06:43 +0300

    Created the order personal details

--- frontend/ozcart/lib/screens/cart_screens/cart_with_products.dart
+++ frontend/ozcart/lib/screens/cart_screens/cart_with_products.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ozcart/screens/cart_screens/oz_cart_product.dart';
+import 'package:ozcart/screens/order_personal_details.dart';
 
 import 'oz_cart_action.dart';
 
@@ -21,14 +22,7 @@ class CartWithProducts extends StatelessWidget {
             ),
           ),
           OzCartAction(
-            onOrder: () => {
-              Get.snackbar(
-                'Order Status',
-                'Placing orders is coming soon!',
-                colorText: Colors.white,
-                backgroundColor: Colors.black.withOpacity(0.8),
-              )
-            },
+            onOrder: () => Get.to(() => OrderPersonalDetails()),
           ),
         ],
       ),
