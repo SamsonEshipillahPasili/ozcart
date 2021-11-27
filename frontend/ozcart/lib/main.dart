e58eb0cc3599cd7e77959a6899a36f8e0b9fe305
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2021-11-27 21:18:21 +0300

    Removed unnecessary imports

--- frontend/ozcart/lib/main.dart
+++ frontend/ozcart/lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:ozcart/screens/edit_account_screen.dart';
-import 'package:ozcart/screens/home_screen.dart';
-import 'package:ozcart/screens/login_screen.dart';
-import 'package:ozcart/screens/order_details_screen.dart';
-import 'package:ozcart/screens/order_placed_screen.dart';
-import 'package:ozcart/screens/product_detail_screen.dart';
-import 'package:ozcart/screens/register_screen.dart';
 import 'package:ozcart/screens/splash_screen.dart';
 import 'package:ozcart/theme/theme.dart';
 
@@ -20,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: theme,
-      home: HomeScreen(),
+      home: SplashScreen(),
       title: 'OzCart',
     );
   }
